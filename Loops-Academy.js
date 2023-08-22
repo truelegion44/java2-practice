fff8b202e723feed1166fbb9460fa4f540cf2bac
nathanielchebem@gmail.com <nathanielchebem@gmail.com>
2023-08-22 13:53:20 -0700

    loops

--- Loops-Academy.js
+++ Loops-Academy.js
@@ -17,4 +17,18 @@ for (let num = 5; num <= 10; num++) {
 
 
 
+//Nested Loops
 
+const bobsFollowers = ['jane', 'kate', 'joy', 'paul'];
+const tinasFollowers = ['jane', 'kate', 'daniel'];
+const mutualFollowers = [];
+
+for (let i = 0; i < bobsFollowers.length; i++) {
+  for (let j = 0; j < tinasFollowers.length; j++) {
+    if (bobsFollowers[i] === tinasFollowers[j]) {
+      mutualFollowers.push(bobsFollowers[i]);
+    }
+  }
+}
+
+console.log("Mutual followers:", mutualFollowers);
--- loop.js
+++ loop.js
@@ -41,3 +41,29 @@ for (let mybaby = 0; mybaby < 1000; mybaby++) {
   const element = 100;[mybaby];
   console.log(mybaby); 
 }
+
+
+// reverse loops 
+
+// The loop below loops from 0 to 3. Edit it to loop backwards from 3 to 0
+for (let counter = 0; counter < 4; counter++){
+  console.log(counter);
+}
+
+for (let counter = 3; counter >= 0; counter--) {
+  console.log(counter);
+}
+
+
+/// Looping through Arrays
+
+
+
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+// Write your code below
+for (let i = 0; i < vacationSpots.length; i++) {
+  console.log('I would love to visit ' + vacationSpots[i]);
+}
+
+
